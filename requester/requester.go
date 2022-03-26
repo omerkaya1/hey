474e839a2ecdf7bff6bf742b962aa5a925a03462
Omer Kaya <omerkaya513@gmail.com>
2022-03-26 08:36:32 +0200

    Use http.Request.Clone() for deep copying requests

--- requester/requester.go
+++ requester/requester.go
@@ -261,16 +261,8 @@ func (b *Work) runWorkers() {
 }
 
 // cloneRequest returns a clone of the provided *http.Request.
-// The clone is a shallow copy of the struct and its Header map.
 func cloneRequest(r *http.Request, body []byte) *http.Request {
-	// shallow copy of the struct
-	r2 := new(http.Request)
-	*r2 = *r
-	// deep copy of the Header
-	r2.Header = make(http.Header, len(r.Header))
-	for k, s := range r.Header {
-		r2.Header[k] = append([]string(nil), s...)
-	}
+	r2 := r.Clone(r.Context())
 	if len(body) > 0 {
 		r2.Body = ioutil.NopCloser(bytes.NewReader(body))
 	}
